5f5393c6848f0c3d08ad40eeb3f98069afafe88b
mfrashad <m.fathyrashad@gmail.com>
2019-07-14 18:08:27 +0800

    Annotate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  email           :string
+#  password_digest :string
+#  name            :string
+#  role            :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
--- test/models/transaction_test.rb
+++ test/models/transaction_test.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: transactions
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer
+#  date       :date
+#  time       :time
+#  role       :string
+#  category   :string
+#  amount     :float
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class TransactionTest < ActiveSupport::TestCase
