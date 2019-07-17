5c359ff6c32187e89422de0d2a09c84f4b37e686
mfrashad <m.fathyrashad@gmail.com>
2019-07-18 02:21:33 +0800

    Update login api

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,11 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_url, notice: "Logged in!"
+      if params[:json]
+        render json: user
+      else
+        redirect_to root_url, notice: "Logged in!"
+      end
     else
       flash.now[:alert] = "Email or password is invalid"
       render "new"
