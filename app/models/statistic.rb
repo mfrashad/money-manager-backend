1b23380b8279a93f075923e9e5442f004145b3e1
mfrashad <m.fathyrashad@gmail.com>
2019-07-18 04:51:17 +0800

    Fix Statistic API

--- app/models/statistic.rb
+++ app/models/statistic.rb
@@ -1,8 +1,8 @@
 class Statistic
   def self.generate(userId)
     transactions = Transaction.getTransactions()
-    date = transactions.where(user: userId).group(:date).sum(:amount)
-    category = transactions.where(user: userId).group(:category).sum(:amount)
+    date = Transaction.where(user: userId).group_by { |m| m.date.strftime("%B") }.map { |m, t| [m, t.reduce(0) { |sum, t| sum + t.amount } ] }
+    category = transactions.where(user: userId).group(:category).sum(:amount).map {|x| x}
     total = transactions.where(user: userId).group(:role).sum(:amount)
     return statistic = {date: date, category: category, total: total}
   end
